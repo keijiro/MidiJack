c1a4f9a486eaf893952b599e0b9ee78b3ba88994
Keijiro Takahashi <keijiro@gmail.com>
2015-06-21 11:05:56 +0900

    Added Visual Studio solution.

---
+++ VisualStudio/MidiJackPlugin/stdafx.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <mmsystem.h>
+
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <mutex>
+#include <queue>
+#include <list>
+#include <stack>
+#include <iostream>
